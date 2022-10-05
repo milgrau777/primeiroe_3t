9b71e7cb23cdcede9001d78c9cd9a675aeb24965
henrique7 <106184359+milgrau777@users.noreply.github.com>
2022-10-05 09:12:16 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -14,4 +14,26 @@ function escreva(){
     document.write(tabuada + " x 10 =  " + (tabuada*10)+"<br>");
 
 };
+                                                            
 
+
+function minhatabuada(){
+    for(let i=0; i < 10; i++){
+        document.write("tabuada do " + 1 + "<br>" );
+        for(let j = 1;~j<= 10; j++){
+          document.write(tabuada + "x" + 1 + " = " + (j*i)+"<br>")
+        }
+    document.write("<br>");
+
+    }
+
+      function quadrado(){
+          for(let i=1; i <=10; i++){
+              Document.write("o quadrado de"+ i + "é" + (i*i) + "<br>");
+
+
+          }}
+          
+  
+ 
+      }
